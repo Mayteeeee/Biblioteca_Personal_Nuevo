b15303327b681875767f2146e8aecb0f74bea712
litzifuentes <litzifuentes486@gmail.com>
2026-06-27 23:27:22 -0600

    CRUD de Libros finalizado y listo para integración

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const conectarDB = require('./config/database');
+const libroRoutes = require('./routes/libroRoutes');
 
 const app = express();
 
@@ -10,6 +11,7 @@ conectarDB();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/libros', libroRoutes);
 
 app.get('/', (req, res) => {
   res.send('API Biblioteca Personal funcionando');
